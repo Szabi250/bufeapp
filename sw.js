8bd6c1f81ac864f6785e2264828207e2fe07f9b9
Szabi250 <155306743+Szabi250@users.noreply.github.com>
2026-09-25 16:00:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BüféApp offline gyorsítótár: csak az app saját fájljait tárolja, az adatbázist soha.
-const CACHE = 'bufeapp-202609251151';
+const CACHE = 'bufeapp-202609251557';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
